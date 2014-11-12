d40a89eb2321871fc65cd27ec81c365d9718e339
Shintaro Kaneko <kaneshin0120@gmail.com>
2014-11-12 13:17:28 +0900

    Initial import

---
+++ GoogleAnalytics-Sample/Objective-C-Bridging-Header.h
@@ -0,0 +1,15 @@
+// Objective-C-Bridging-Header.h
+//
+// Copyright (c) 2014 Shintaro Kaneko
+
+#ifndef GoogleAnalytics_Sample_Objective_C_Bridging_Header
+#define GoogleAnalytics_Sample_Objective_C_Bridging_Header
+
+// Use this file to import your target's public headers that you would like to expose to Swift.
+
+#import <GoogleAnalytics-iOS-SDK/GAI.h>
+#import <GoogleAnalytics-iOS-SDK/GAIFields.h>
+#import <GoogleAnalytics-iOS-SDK/GAILogger.h>
+#import <GoogleAnalytics-iOS-SDK/GAIDictionaryBuilder.h>
+
+#endif /* GoogleAnalytics_Sample_Objective_C_Bridging_Header */
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+xcodeproj 'GoogleAnalytics-Sample'
+pod 'GoogleAnalytics-iOS-SDK', '3.10'
+
